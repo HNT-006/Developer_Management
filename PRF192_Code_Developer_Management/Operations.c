4ec38bebf8672faae3f2ba5551a6baf57a34436e
Nguyen Hoang Duy <nguyenhoangduy20.5.2006@gmail.com>
2026-03-12 13:39:44 +0700

    Duy: Modify in Operations file

---
+++ PRF192_Code_Developer_Management/Operations.c
@@ -0,0 +1,52 @@
+#include "Operation.h"
+#include "Common.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*========================= findDevByName function =========================*/
+// Trả về: index nếu tìm thấy | -1 nếu không tìm thấy | -2 nếu Name không hợp lệ
+int findDevByName(char Name[], Developer developers[], int DevCount)
+{
+    // Kiểm tra format tên
+    if (!validName(Name))
+    {
+        return -2; // Name không hợp lệ
+    }
+
+    for (int i = 0; i < DevCount; i++)
+    {
+        if (strcmp(developers[i].Name, Name) == 0)
+        {
+            return i; // Tìm thấy, trả về vị trí
+        }
+    }
+
+    return -1; // Không tìm thấy
+}
+
+
+/*========================= findDevByID function =========================*/
+// Trả về: index nếu tìm thấy | -1 nếu không tìm thấy | -2 nếu ID không hợp lệ
+int findDevByID(char ID[], Developer developers[], int DevCount)
+{
+    // Kiểm tra format ID
+    if (!validID(ID))
+    {
+        return -2; // ID không hợp lệ
+    }
+
+    // Duyệt mảng từ 0 -> DevCount - 1
+    for (int i = 0; i < DevCount; i++)
+    {
+        if (strcmp(developers[i].ID, ID) == 0)
+        {
+            return i; // Tìm thấy, trả về vị trí
+        }
+    }
+
+    return -1; // Không tìm thấy
+}
+
+/*========================= validBirthDay function =========================*/
+
