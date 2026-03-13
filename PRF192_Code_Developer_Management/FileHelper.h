9a3bf9e7af7fea683d61549db4554b6191295e41
thaibinh352006-ship-it <thaibinh352006@gmail.com>
2026-03-13 13:54:14 +0700

    BINH: LAM FILEHELPER.H

---
+++ PRF192_Code_Developer_Management/FileHelper.h
@@ -0,0 +1,22 @@
+#ifnded FILE_HELPER_H
+#define FILE_HELPER_H
+
+#include"global.h"
+
+//===================== DEVELOPER FILE HANDLING =====================
+
+// Save all developers to file
+void saveDevelopersToFile();
+
+// Load developers from file
+void loadDevelopersFromFile();
+
+//===================== PROJECT FILE HANDLING =======================
+
+// Save all projects to file
+void saveProjectsToFile();
+
+// Load projects from file
+void loadProjectsFromFile();
+
+#endif
